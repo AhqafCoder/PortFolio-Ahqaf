24af66676c950cdfa3fe21ce6c4ee4fe2a1b72b0
Ahqaf Ali <ahqafaliofficial@gmail.com>
2025-02-07 18:30:06 +0000

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
 const Hero = () => {
   return (
     <div className="h-screen flex items-center justify-center bg-[url('./assets/bg.png')] bg-cover bg-center">
+      <div>
       <h1 className="text-white text-5xl font-bold">Welcome to My Site</h1>
+      </div>
+      
     </div>
   );
 };
+
 
 export default Hero;
