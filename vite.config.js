8357326b3061465e3056309241357c73ae1752d9
Ahqaf Ali <ahqafaliofficial@gmail.com>
2025-02-11 15:16:37 +0530

    updated vite.config.js for allowedhosts

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 10000,
+    strictPort: true,
+    allowedHosts: ["portfolio-ahqaf.onrender.com"] // Add this line
+  }
   
 })
 
