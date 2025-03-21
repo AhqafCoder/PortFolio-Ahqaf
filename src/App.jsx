33b1694cd204abd160a9361be1b02b023cd0f2f7
Ahqaf Ali <ahqafaliofficial@gmail.com>
2025-03-21 05:09:08 +0000

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import "./index.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
   return (
@@ -16,6 +17,8 @@ export default function App() {
       <Projects />
       <Contact />
       <Footer />
+      <Analytics />
+
     </div>
   );
 }
