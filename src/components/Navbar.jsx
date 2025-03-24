2735e036ad750d7f860991ed0aee54128191e29b
Ahqaf Ali <ahqafaliofficial@gmail.com>
2025-03-24 23:55:52 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,6 +53,7 @@ const Navbar = () => {
                             </a>
                         ))}
                     </div>
+                    
                 </div>
 
                 {/* Desktop Button */}
